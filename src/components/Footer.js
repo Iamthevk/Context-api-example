7bfc3d31389a491fa0cb435adcd67dbd1c01d33a
Vijay Kumar <vijay.vuex@gmail.com>
2022-09-28 17:39:19 +0530

    Footer & Main component created

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from './Card'
+
+function Footer() {
+    return (
+        <Card >Footer</Card>
+    )
+}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+
+function Main() {
+    return (
+        <>
+            <Card>Hero Card</Card>
+            <Card>Details Card</Card>
+        </>
+    )
+}
+
+export default Main
