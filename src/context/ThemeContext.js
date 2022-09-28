0d1b193f34ae2de5ce23ed8d714dfbbb75c13914
Vijay Kumar <vijay.vuex@gmail.com>
2022-09-28 17:38:29 +0530

    theme context created

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+function ThemeProvider(props) {
+
+    const [theme, setTheme] = useState("dark")
+    function toggleTheme() {
+        if (theme === "dark") {
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+    }
+    const value = {
+        theme: theme,
+        toggleTheme: toggleTheme
+    }
+    return (
+        <ThemeContext.Provider value={value}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
+export { ThemeContext, ThemeProvider }
